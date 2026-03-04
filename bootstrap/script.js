dc0a11244437ef27d2d92b9a49d1caa6147ec321
Ajmal-Noushad <ajmalaxj@gmail.com>
2026-03-04 11:16:52 +0530

    added screenshot

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -1,29 +1,54 @@
-let globalName = "John";
-
-function showName() {
-    let localName = "Doe";
-
-    console.log(globalName);
-    console.log(localName);
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed
 }
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
 
-showName();
-
-let sum = 10 + 5;
-console.log(10>sum);
-let access = isAdult && hasLicense;
-let x = 10; x += 5;
-
-let time = 20;
-
-if (time<12) {
-    console.log("Good Morning");
+function isPrime(num) {
+    if (num<=1) {
+        return false;
+    }
+    for (let i=2; i<=Math.sqrt(num); i++) {
+        if (num%i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+console.log(isPrime(11));
+console.log(isPrime(12));
 
-else if (time<18) {
-    console.log("Good Afternoon");
+function reverseString(str) {
+    return str.split("").reverse().join("");
 }
+console.log(reverseString("hello"))
+function factorial(n) {
+    let result = 1;
+    for (let i=1 ; i<=n ; i++) {
+        result *= i;
+    }
+    return result;
+}
+console.log(factorial(5));
 
-else {
-    console.log("Good Evening");
+function findLargest(arr) {
+    let largest = arr[0];
+    for(let i=0 ; i<arr.length ; i++) {
+        if(arr[i]>largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
 }
+console.log(findLargest([3, 7, 2, 9, 5]));
+
+function star_triangle(n) {
+    for (i=1 ; i<=n ; i++) {
+        for (j=1 ; j<=i ; j++) {
+            console.log("*");
+        }
+        console.log("\n")
+    }
+}
+console.log(star_triangle(5));
