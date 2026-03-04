0c2ce1bbd8a42c8502803551d695cf265aa8e09f
Ajmal-Noushad <ajmalaxj@gmail.com>
2026-03-04 10:36:15 +0530

    Kaira static page

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -13,3 +13,17 @@ let sum = 10 + 5;
 console.log(10>sum);
 let access = isAdult && hasLicense;
 let x = 10; x += 5;
+
+let time = 20;
+
+if (time<12) {
+    console.log("Good Morning");
+}
+
+else if (time<18) {
+    console.log("Good Afternoon");
+}
+
+else {
+    console.log("Good Evening");
+}
